272c7925b38c35ea4972436ebdc6c41a41509ef6
jessicaborsetto <bjessi2002@gmail.com>
2024-01-12 17:00:54 +0100

    epicode_exam

--- settimana7/EpiFlix/src/components/Movies.jsx
+++ settimana7/EpiFlix/src/components/Movies.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// const Movies = (props) => {
-//     return(
-//         <>
-//         {props.movies.map((movie, index) =>(
-//             <img className="moviePoster" src={movie.Poster} alt="movie" key={index}></img>
-//         )
-//         )}
-//         </>
-//     )
-// }
-
-// export default Movies
-
-
 import React from "react";
 import { Carousel } from 'react-bootstrap';
 
