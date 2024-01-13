ae703234837c75991fb92dd4dcbd2b6046d5db56
jessicaborsetto <bjessi2002@gmail.com>
2024-01-13 16:13:08 +0100

    exam_epicode

--- settimana7/EpiFlix/src/components/Movies.jsx
+++ settimana7/EpiFlix/src/components/Movies.jsx
@@ -5,6 +5,9 @@ const Movies = (props) => {
     const moviesPerRow = 5;
     const items = [];
 
+    // stato per far mostrare il titolo all'hover del mouse
+    const [hoveredMovie, setHoveredMovie] = React.useState(null);
+    
     for (let i = 0; i < props.movies.length; i += moviesPerRow) {
         const movieGroup = props.movies.slice(i, i + moviesPerRow);
 
@@ -12,12 +15,20 @@ const Movies = (props) => {
             <Carousel.Item key={i} className="movie-carousel-item movieCarousel">
                 <div className="d-flex justify-content-between movieCarousel">
                     {movieGroup.map((movie, index) => (
-                        <div key={index} className="movie-poster-container">
+                        <div key={index} className="movieContainer" onMouseOver={() => setHoveredMovie(movie.Title)}
+                        onMouseOut={() => setHoveredMovie(null)}>
                             <img
                                 src={movie.Poster}
                                 alt={movie.Title}
                                 className="movie-poster"
                             />
+                             {/* c'è l'hover? allora mostra il titolo*/}
+                             {hoveredMovie === movie.Title && (
+                                <div className="movieTitle">
+                                    {movie.Title}
+                                </div>
+                            )}
+
                         </div>
                     ))}
                 </div>
